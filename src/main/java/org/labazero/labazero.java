b7424b618103bfb98bd6df6e18149fb6e747d041
thecoocko <butenkonaddezhda@gmail.com>
2022-09-09 12:40:36 +0200

    fix: change branch

--- src/main/java/org/labazero/labazero.java
+++ src/main/java/org/labazero/labazero.java
@@ -2,7 +2,7 @@ package org.labazero;
 
 public class labazero {
         public static void helloWorld() {
-            System.out.println("Hello world!DDFSDfsdfsdf");
+            System.out.println("Hello world!");
         }
 
 }
