62bb6c56a92d82d2a94df5c3c44eb2727a686682
thecoocko <butenkonaddezhda@gmail.com>
2022-09-26 22:04:03 +0200

    added: lab3

---
+++ src/main/java/org/labathree/Human.java
@@ -0,0 +1,32 @@
+package org.labathree;
+
+public abstract class Human {
+    enum sex {male,female,diverse}
+    protected String name;
+    protected String lastName;
+    protected String patronymic;
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+}
